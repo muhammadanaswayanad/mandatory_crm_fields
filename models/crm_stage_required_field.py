af7d0836411fa590ef7849404b61bb3594be9e9e
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-05-31 12:31:58 +0530

    Add ondelete cascade option to field_id in CrmStageRequiredField model

--- models/crm_stage_required_field.py
+++ models/crm_stage_required_field.py
@@ -8,6 +8,7 @@ class CrmStageRequiredField(models.Model):
     field_id = fields.Many2one('ir.model.fields', string='Field', required=True, 
                                domain=[('model', '=', 'crm.lead'), 
                                        ('ttype', 'not in', ['binary', 'boolean', 'one2many', 'many2many'])],
+                               ondelete='cascade',
                                help="The field that will be required in this stage")
     field_name = fields.Char(related='field_id.name', string='Field Name', readonly=True, store=True)
     field_description = fields.Char(related='field_id.field_description', string='Field Label', readonly=True)
